4dfc23c119dab9ac4899b7723c069539ae84ba05
anambiath <akhil.nambiath@orbitz.com>
2015-12-10 03:00:30 -0600

    Added the controllers in the hotelreview package

---
+++ hotel-review/src/main/java/com/egencia/hotel/HotelReveiw.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+import java.lang.String;
+
+
+public class HotelReview extends SpringBootServletInitializer{
+    public static void main(String[] args){
+        SpringApplication.run(HotelReview.class, args);
+    }
+}
